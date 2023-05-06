180dcb06acf62faf95ae4c495baf710860a251ee
kumilachew-g <kumilachew1212@gmail.com>
2023-05-06 11:46:54 +0300

    Add validations for password

--- src/validations/formValidation.js
+++ src/validations/formValidation.js
@@ -16,3 +16,23 @@ export const usernameValidate = {
     message: "Username must not contain special characters",
   },
 };
+
+export const passwordValidate = {
+  required: {
+    value: true,
+    message: "Password is required",
+  },
+  minLength: {
+    value: 8,
+    message: "Password must be at least 8 characters",
+  },
+  maxLength: {
+    value: 20,
+    message: "Password must not exceed 20 characters",
+  },
+  pattern: {
+    value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/,
+    message:
+      "Password must contain at least 1 uppercase, 1 lowercase and 1 number",
+  },
+};
