2e6a5507d01c637e85087ee27c9ef83f2ee64165
kumilachew-g <kumilachew1212@gmail.com>
2023-05-06 10:33:22 +0300

    Add routes for home page

--- src/config/Routes.jsx
+++ src/config/Routes.jsx
@@ -1,8 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import Login from "../components/auth/LogIn";
+import Register from "../components/auth/Register";
+import Home from "../components/layout";
 
+export const ROOT = "/";
 export const LOGIN = "/login";
+export const REGISTER = "/register";
 
 export const router = createBrowserRouter([
+  { path: ROOT, element: <Home /> },
   { path: LOGIN, element: <Login /> },
+  { path: REGISTER, element: <Register /> },
 ]);
