5385ba0cd98bd62adeb5ef41458dfcdaa29332a8
kumilachew-g <kumilachew1212@gmail.com>
2023-05-07 10:40:25 +0300

    Add box element with an attribute and values

--- src/components/pages/Footer.jsx
+++ src/components/pages/Footer.jsx
@@ -8,4 +8,11 @@ import {
 } from "@chakra-ui/react";
 import { FaGithub } from "react-icons/fa";
 
-export default function Footer() {}
+export default function Footer() {
+  return (
+    <Box
+      bg={useColorModeValue("gray.50", "gray.900")}
+      color={useColorModeValue("gray.700", "gray.200")}
+    ></Box>
+  );
+}
