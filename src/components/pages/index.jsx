c5244ae4457e29ac715aed2248c9eecf515fad79
kumilachew-g <kumilachew1212@gmail.com>
2023-05-08 18:12:50 +0300

    Add Home function for the layout pages

--- src/components/pages/index.jsx
+++ src/components/pages/index.jsx
@@ -2,3 +2,13 @@ import React from "react";
 import Footer from "./Footer";
 import NavBar from "./NavBar";
 import PostLists from "./PostLists";
+
+export default function Home() {
+  return (
+    <div>
+      <NavBar />
+      <PostLists />
+      <Footer />
+    </div>
+  );
+}
