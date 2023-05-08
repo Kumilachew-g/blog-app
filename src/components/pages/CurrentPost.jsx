c99e0d7b86a09951a06a43f904d1b05d0c92deef
kumilachew-g <kumilachew1212@gmail.com>
2023-05-08 17:41:52 +0300

    Declare and implement useParams, useState and useEffect

--- src/components/pages/CurrentPost.jsx
+++ src/components/pages/CurrentPost.jsx
@@ -19,4 +19,12 @@ import { AiOutlineRollback } from "react-icons/ai";
 import { ROOT } from "../../config/routes";
 import NavBar from "./NavBar";
 
-export default function CurrentPost() {}
+export default function CurrentPost() {
+  const { postId } = useParams();
+  const [currentPost, setCurrentPost] = useState([]);
+  useEffect(() => {
+    onSnapshot(doc(db, "posts", postId), (snapshot) => {
+      setCurrentPost({ ...snapshot.data(), id: snapshot.id });
+    });
+  }, []);
+}
