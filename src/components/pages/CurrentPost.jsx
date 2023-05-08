b86a5f15849ae74e6989d09e07728a41445b619e
kumilachew-g <kumilachew1212@gmail.com>
2023-05-08 17:43:45 +0300

    Add CurrentPost design layout page

--- src/components/pages/CurrentPost.jsx
+++ src/components/pages/CurrentPost.jsx
@@ -27,4 +27,68 @@ export default function CurrentPost() {
       setCurrentPost({ ...snapshot.data(), id: snapshot.id });
     });
   }, []);
+
+  console.log(currentPost);
+
+  return (
+    <>
+      <NavBar />
+      <motion.div layout>
+        <Container maxW={"7xl"} p="12">
+          <motion.button
+            whileHover={{
+              scale: 1.2,
+              transition: { duration: 1 },
+            }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <IconButton
+              colorScheme="#319594"
+              as={routerLink}
+              to={ROOT}
+              size="lg"
+              icon={<AiOutlineRollback />}
+              isRound
+              variant="ghost"
+            />
+          </motion.button>
+          <Heading as="h2">{currentPost.title}</Heading>
+
+          <Divider marginTop="5" />
+          <Grid
+            templateColumns="repeat(auto-fill, minmax(100%, 1fr))"
+            gap={6}
+            marginTop="5"
+          >
+            <GridItem>
+              <Box w="100%">
+                <Box borderRadius="lg" overflow="hidden">
+                  <Link
+                    textDecoration="none"
+                    _hover={{ textDecoration: "none" }}
+                  >
+                    <Image
+                      transform="scale(1.0)"
+                      src={currentPost.imageUrl}
+                      alt="blog image here"
+                      width="100%"
+                      height={"60vh"}
+                      objectFit="cover"
+                      transition="0.3s ease-in-out"
+                      _hover={{
+                        transform: "scale(1.05)",
+                      }}
+                    />
+                  </Link>
+                </Box>
+                <Text as="p" fontSize="md" marginTop="2">
+                  {currentPost.desc}
+                </Text>
+              </Box>
+            </GridItem>
+          </Grid>
+        </Container>
+      </motion.div>
+    </>
+  );
 }
