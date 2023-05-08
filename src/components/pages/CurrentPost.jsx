96b195c83b171f777f8815ba7f2b898d56de4d55
kumilachew-g <kumilachew1212@gmail.com>
2023-05-08 17:25:14 +0300

    Add layout adn staying attributes from @chakra-ui/react

--- src/components/pages/CurrentPost.jsx
+++ src/components/pages/CurrentPost.jsx
@@ -1,1 +1,13 @@
 import React, { useEffect, useState } from "react";
+import {
+  Box,
+  Heading,
+  Link,
+  Image,
+  Text,
+  Container,
+  Divider,
+  Grid,
+  GridItem,
+  IconButton,
+} from "@chakra-ui/react";
