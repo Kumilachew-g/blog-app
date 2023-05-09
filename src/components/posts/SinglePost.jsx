85043952c103a81deac33380d799e969262afda4
kumilachew-g <kumilachew1212@gmail.com>
2023-05-09 17:16:26 +0300

    Add and implement hooks methods

--- src/components/posts/SinglePost.jsx
+++ src/components/posts/SinglePost.jsx
@@ -16,4 +16,15 @@ import { useAuth } from "../../hooks/auths";
 import { useDeletePost, useToggleLike } from "../../hooks/posts";
 import { useUser } from "../../hooks/user";
 
-const SinglePost = ({ post }) => {};
+const SinglePost = ({ post }) => {
+  const { user, isLoading: authLoading } = useAuth();
+  const { id, likes, uid } = post;
+  const isLiked = likes.includes(user?.id);
+  const { toggleLike, isLoading } = useToggleLike({
+    id,
+    isLiked,
+    uid: user?.id,
+  });
+  const { deletePost, isLoading: deleteLoading } = useDeletePost(id);
+  const { user: users, isLoading: userLoading } = useUser(uid);
+};
