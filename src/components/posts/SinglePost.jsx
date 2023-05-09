fa969550c8da97a0b2a8a3749182446519406b14
kumilachew-g <kumilachew1212@gmail.com>
2023-05-09 17:16:59 +0300

    Add SinglePost layout page

--- src/components/posts/SinglePost.jsx
+++ src/components/posts/SinglePost.jsx
@@ -27,4 +27,71 @@ const SinglePost = ({ post }) => {
   });
   const { deletePost, isLoading: deleteLoading } = useDeletePost(id);
   const { user: users, isLoading: userLoading } = useUser(uid);
+
+  return (
+    <>
+      <Box w="100%" key={post.id}>
+        <Box borderRadius="lg" overflow="hidden">
+          <Link textDecoration="none" _hover={{ textDecoration: "none" }}>
+            <Image
+              transform="scale(1.0)"
+              src={post.imageUrl}
+              alt="some text"
+              width="100%"
+              objectFit="cover"
+              height={"400px"}
+              transition="0.3s ease-in-out"
+              _hover={{
+                transform: "scale(1.05)",
+              }}
+            />
+          </Link>
+        </Box>
+        <Heading fontSize="xl" marginTop="2">
+          <Link
+            textDecoration="none"
+            _hover={{ textDecoration: "none" }}
+            as={routerLink}
+            to={`/posts/${post?.id}`}
+          >
+            {post.title}
+          </Link>
+        </Heading>
+        <Text as="p" fontSize="md" marginTop="2">
+          {post.desc.substring(0, 150)}...
+        </Text>
+        <Box mt={"10px"}>
+          <Flex align={"center"}>
+            <Avatar name={users?.username} size={"sm"} />
+            <Text casing={"capitalize"}>
+              <span style={{ paddingLeft: "10px" }}>
+                {formatDistanceToNow(post.date)} ago
+              </span>
+            </Text>
+            <IconButton
+              colorScheme="red"
+              onClick={toggleLike}
+              isLoading={authLoading || isLoading}
+              size="md"
+              icon={isLiked ? <AiTwotoneHeart /> : <AiOutlineHeart />}
+              isRound
+              variant="ghost"
+            />
+            <Text> {likes.length}</Text>
+            {!authLoading && user?.id === uid && (
+              <IconButton
+                colorScheme="red"
+                size="lg"
+                icon={<AiFillDelete />}
+                isRound
+                onClick={deletePost}
+                isLoading={deleteLoading}
+                variant="ghost"
+              />
+            )}
+          </Flex>
+        </Box>
+      </Box>
+    </>
+  );
 };
